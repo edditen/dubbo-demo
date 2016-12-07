6aa045b8db6880111f0968cc4f073863883ec635
tenchael <hztengzz@163.com>
2016-12-07 17:46:58 +0800

    temp2

--- dubbo-api/src/main/java/com/tenchael/dubbo/api/DemoService.java
+++ dubbo-api/src/main/java/com/tenchael/dubbo/api/DemoService.java
@@ -3,8 +3,6 @@ package com.tenchael.dubbo.api;
 import java.io.Serializable;
 import java.util.List;
 
-import com.tenchael.dubbo.bean.User;
-
 public interface DemoService {
 	String sayHello(String msg);
 	
