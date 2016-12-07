b511d5a14a2987cff0ea76f81c09cde3e832ab27
tenchael <hztengzz@163.com>
2016-12-07 18:24:15 +0800

    tmp 3

--- dubbo-web/src/main/java/com/tenchael/dubbo/web/controller/HomeController.java
+++ dubbo-web/src/main/java/com/tenchael/dubbo/web/controller/HomeController.java
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 @Controller
 public class HomeController {
