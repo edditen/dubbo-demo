0e23c79afa3aacc74250dc37f11898605b03808f
tenchael <hztengzz@163.com>
2019-10-26 22:44:49 +0800

    remove modules

--- dubbo-api/src/main/java/com/tenchael/dubbo/api/DemoService.java
+++ dubbo-api/src/main/java/com/tenchael/dubbo/api/DemoService.java
@@ -1,16 +1,6 @@
 package com.tenchael.dubbo.api;
 
-import java.io.Serializable;
-import java.util.List;
-
 public interface DemoService {
-	String sayHello(String msg);
-	
-	<T extends Serializable> void set(T user);
-
-	<T extends Serializable> List<T> get();
-
-	<T extends Serializable> T get2();
-
+    String sayHello(String msg);
 
 }
